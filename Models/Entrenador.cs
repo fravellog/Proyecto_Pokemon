e11a8e779a8f14390a33345465333178f457eb2c
fravellog <fr.avellog@duocuc.cl>
2025-05-29 01:33:53 -0400

    Se agrego el campo de pocion

--- Models/Entrenador.cs
+++ Models/Entrenador.cs
@@ -1,8 +1,10 @@
 namespace Pokedex.Models
 {
-    public class Entrenador 
+    public class Entrenador
     {
-        public string Nombre {get; set;}
+        public string Nombre { get; set; }
+
+        public List <Pokebola> Pokebolas { get; } = new List<Pokebola>();
         public List<Pocion> Pociones { get; } = new List<Pocion>();
         public List<Pokemon> Equipo { get; } = new List<Pokemon>();
         public Entrenador(string nombre)
@@ -11,11 +13,34 @@ namespace Pokedex.Models
             Pociones = new List<Pocion>();
             Equipo = new List<Pokemon>();
         }
-        public void agregarPokemon(Pokemon pokemon) 
+        public void agregarPokemon(Pokemon pokemon)
         {
-            if (Equipo.Count < 1) 
+            if (Equipo.Count < 1)
             {
                 Equipo.Add(pokemon);
+            }
+        }
+        public void agregarPocion(Pocion pocion)
+        {
+            if (Pociones.Count < 3)
+            {
+                Pociones.Add(pocion);
+            }
+            else
+            {
+                Console.WriteLine("¡No puedes llevar más pociones!");
+            }
+        }
+        
+        public void agregarPokebola(Pokebola pokebola)
+        {
+            if (Pokebolas.Count < 5)
+            {
+                Pokebolas.Add(pokebola);
+            }
+            else
+            {
+                Console.WriteLine("¡No puedes llevar más pokebolas!");
             }
         }
     }
