8230099e3257da96b6f9dafca8bc4e44a08b831d
fravellog <fr.avellog@duocuc.cl>
2025-05-29 10:18:37 -0400

    Arreglo de metodos

--- Controllers/EntrenadorController.cs
+++ Controllers/EntrenadorController.cs
@@ -6,17 +6,16 @@ namespace Pokedex.Controllers
 {
     public class EntrenadorController
     {
-        public void CrearEntrenador()
+        public void CrearEntrenador(Entrenador entrenador)
         {
             AsciiView.Textos(1);
             Console.WriteLine("¡Ingrese su nombre de entrenador!:");
             Console.WriteLine("");
-            string nombre = string.Empty;
             while (true)
             {
                 Console.WriteLine("Recuerda que el nombre no puede estar vacío.");
-                nombre = Console.ReadLine();
-                if (!string.IsNullOrWhiteSpace(nombre))
+                entrenador.Nombre = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(entrenador.Nombre))
                 {
                     break;
                 }
@@ -24,9 +23,8 @@ namespace Pokedex.Controllers
             }
 
             ConsolaUtil.EsperaryLimpiar();
-            Entrenador entrenador = new Entrenador(nombre);
             AsciiView.Textos(2);
-            Console.WriteLine($"¡Bienvenido entrenador {nombre}!");
+            Console.WriteLine($"¡Bienvenido entrenador {entrenador.Nombre}!");
             ConsolaUtil.EsperaryLimpiar();
         }
     }
